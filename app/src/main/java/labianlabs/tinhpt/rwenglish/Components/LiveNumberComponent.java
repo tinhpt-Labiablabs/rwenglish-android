a22498e8b381e349900ee1dfa2fe877d5c1fcd74
217prod <217prod@gmail.com>
2018-11-04 20:42:07 +0700

    add flow

---
+++ app/src/main/java/labianlabs/tinhpt/rwenglish/Components/LiveNumberComponent.java
@@ -0,0 +1,14 @@
+package labianlabs.tinhpt.rwenglish.Components;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class LiveNumberComponent extends LinearLayout{
+
+
+    public LiveNumberComponent(Context context) {
+        super(context);
+    }
+
+
+}
